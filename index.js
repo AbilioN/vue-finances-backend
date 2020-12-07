5d0221b42b9ccf73ab9bcb342d67071dc023a8ac
= <=>
2020-12-06 23:07:09 -0300

    server starded for protected graphQl Api server

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const { prisma } = require('./generated/prisma-client');
+const { GraphQLServer } = require('graphql-yoga');
 
-async function main(){
 
-    await prisma.createUser({
-        name: 'Abilio Neto',
-        email: 'abilio@email.com',
-        password: '123456'
-    })
-
-    const users = await prisma.users();
-
-    console.log( 'Users : ' , users);
+const resolvers  = {
+    Query: {
+        user(parent, args, context, info){
+            return prisma.user({id: args.id});
+        }
+    }
 }
 
-main().catch(e => console.error(e));
 
+
+const server = new GraphQLServer({
+    typeDefs: './schema.graphql',
+    resolvers: resolvers
+});
+
+
+
+server.start().then(() => console.log('server running on http://localhost:4000...'));
